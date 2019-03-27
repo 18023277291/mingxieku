d11689a4be0c0cf5f301339d2810c262819fae35
liushiqi647 <2422851183@qq.com>
2019-03-27 13:58:43 +0800

    public html

--- app/js/a.js
+++ app/js/a.js
@@ -1,10 +1,10 @@
 var gulp=require('gulp');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-var connect = require('gulp-connect');
+connect = require('gulp-connect');
 gulp.task('conect', function() {
   connect.server({
-    root: 'dist',
+    root: 'app',
     livereload: true,
 		port:8888
   });
@@ -12,28 +12,23 @@ gulp.task('conect', function() {
 gulp.task('allfile',function(){ //加载所有文件
 	gulp.src(['app/**/*','!app/css/*.scss'])
 	.pipe(gulp.dest('dist'))
-	.pipe(connect.reload());
 });
 gulp.task('Sass', function () { //css代码预编译
 	return gulp.src('app/css/*.scss')
     .pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(connect.reload());
+    .pipe(gulp.dest('dist/css'));
 });
 gulp.task('html',function(){ //加载所有文件
 	gulp.src('app/branchPage/*.html')
 	.pipe(gulp.dest('dist/branchPage'))
-	.pipe(connect.reload());
 });
 gulp.task('indexHtml',function(){ //首页监听
 	gulp.src('app/*.html')
 	.pipe(gulp.dest('dist/'))
-	.pipe(connect.reload());
 });
 gulp.task('js',function(){	//js代码监听
-	gulp.src('app/js/*.js')
-	.pipe(gulp.dest('dist/js'))
-	.pipe(connect.reload());
+	gulp.src('app/javascript/*.js')
+	.pipe(gulp.dest('dist/javascript'))
 })
 gulp.task('ajaxApi',function(){	//api接口代码监听
 	gulp.src('app/ajaxApi/*.js')
@@ -48,18 +43,18 @@ gulp.task('commejs',function(){	//api接口代码监听
 	.pipe(gulp.dest('dist/comme.js'))
 })
 gulp.task('imgchange',function(){	//api接口代码监听
-	gulp.src('app/images/*')
-	.pipe(gulp.dest('dist/images'))
+	gulp.src('app/imags/*')
+	.pipe(gulp.dest('dist/imags'))
 })
 gulp.task('htmlWatch',function(){//监听css改变事件
 	gulp.watch('app/branchPage/*.html',['html'])
 	gulp.watch('app/*.html',['indexHtml'])
 	gulp.watch('app/css/*.scss',['Sass'])
-	gulp.watch('app/js/*.js',['js'])
+	gulp.watch('app/javascript/*.js',['js'])
 	gulp.watch('app/ajaxApi/*.js',['ajaxApi'])
 	gulp.watch('app/comme.css/*.css',['commecss'])
 	gulp.watch('app/comme.js/*.js',['commejs'])
-	gulp.watch('app/images/*',['imgchange'])
+	gulp.watch('app/imags/*',['imgchange'])
 });
 
 gulp.task('default',['allfile','Sass','conect','htmlWatch'])
---
+++ app/js/nav_erji.js
@@ -0,0 +1,118 @@
+$(function () {
+    $('.if_cat').on('mouseenter', function () {
+        $('.suoyou_menu').css("display", "block")
+    })
+    $('.if_cat').on('mouseleave', function () {
+        $('.suoyou_menu').css("display", "none")
+    })
+    $('.suoyou_menu').on('mouseenter', function () {
+        $('.suoyou_menu').css("display", "block")
+    })
+    $('.suoyou_menu').on('mouseleave', function () {
+        $('.suoyou_menu').css("display", "none")
+    })
+    $('.menu_nav li').on('mouseenter', function () {
+        $('.category-sub').css("display", "block")
+    })
+    $('.menu_nav li').on('mouseleave', function () {
+        $('.category-sub').css("display", "none")
+    })
+    $('.nanzi').on('mouseenter', function () {
+        $('.nanzi_nav').css("display", "block")
+    })
+    $('.nanzi').on('mouseleave', function () {
+        $('.nanzi_nav').css("display", "none")
+    }),
+        $('.nanzi_nav').on('mouseenter', function () {
+            $('.nanzi_nav').css("display", "block")
+        })
+    $('.nanzi_nav').on('mouseleave', function () {
+        $('.nanzi_nav').css("display", "none")
+    })
+    $('.nvzi').on('mouseenter', function () {
+        $('.nvzi_nav').css("display", "block")
+    })
+    $('.nvzi').on('mouseleave', function () {
+        $('.nvzi_nav').css("display", "none")
+    }),
+        $('.nvzi_nav').on('mouseenter', function () {
+            $('.nvzi_nav').css("display", "block")
+        })
+    $('.nvzi_nav').on('mouseleave', function () {
+        $('.nvzi_nav').css("display", "none")
+    })
+    $('.ertong').on('mouseenter', function () {
+        $('.ertong_nav').css("display", "block")
+    })
+    $('.ertong').on('mouseleave', function () {
+        $('.ertong_nav').css("display", "none")
+    }),
+        $('.ertong_nav').on('mouseenter', function () {
+            $('.ertong_nav').css("display", "block")
+        })
+    $('.ertong_nav').on('mouseleave', function () {
+        $('.ertong_nav').css("display", "none")
+    })
+    $('.gouwu').on('mouseenter', function () {
+        $('.gwc').css("display", "block")
+    })
+    $('.gouwu').on('mouseleave', function () {
+        $('.gwc').css("display", "none")
+    })
+    $('.wodedingdan').on('mouseenter', function () {
+        $('.dingdan_2').css("display", "block")
+    })
+    $('.wodedingdan').on('mouseleave', function () {
+        $('.dingdan_2').css("display", "none")
+    })
+    $('.kefu').on('mouseenter', function () {
+        $('.kf_2').css("display", "block")
+    })
+    $('.kefu').on('mouseleave', function () {
+        $('.kf_2').css("display", "none")
+    })
+    $('.wodeshoucang').on('mouseenter', function () {
+        $('.wdsc_2').css("display", "block")
+    })
+    $('.wodeshoucang').on('mouseleave', function () {
+        $('.wdsc_2').css("display", "none")
+    })
+    $('.fenxiang').on('mouseenter', function () {
+        $('.fx_2').css("display", "block")
+    })
+    $('.fenxiang').on('mouseleave', function () {
+        $('.fx_2').css("display", "none")
+    })
+    $('.fanhuitop').on('mouseenter', function () {
+        $('.fht_2').css("display", "block")
+    })
+    $('.fanhuitop').on('mouseleave', function () {
+        $('.fht_2').css("display", "none")
+    })
+})
+var backButton = $('.fanhuitop');
+function backToTop() {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 800);
+}
+backButton.on('click', backToTop);
+
+$(window).on('scroll', function () {/*当滚动条的垂直位置大于浏览器所能看到的页面的那部分的高度时，回到顶部按钮就显示 */
+    if ($(window).scrollTop() > $(window).height())
+        backButton.fadeIn();
+    else
+        backButton.fadeOut();
+});
+$(window).trigger('scroll');
+// 移动；
+$(document).ready(function(){
+    $(".gouwuche").mouseenter(function(){
+      var div=$(".gwc_2");
+      div.stop().animate({right:"37px",},"slow");
+    });
+    $(".gouwuche").mouseleave(function(){
+      var div=$(".gwc_2");
+      div.css("right","47px")
+    });
+});
