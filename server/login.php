c9a0b6dcb68f55d3f81691b628106f3c1d8ac214
CSDN id <2252333248@qq.com>
2019-03-28 17:27:40 +0800

    数据修改

--- server/login.php
+++ server/login.php
@@ -1,5 +1,6 @@
 <?php
  include('connet_db.php');
+ header('Access-Control-Allow-Origin:*');
 $tel = $_POST['tel'];
 $password = $_POST['password'];
 $sql = "select * from mingxieku where tel='$tel' and password='$password' ";
@@ -9,19 +10,13 @@ if($resulf){
  echo"
       <script>
       alert('登录成功');
-      location.href='#';
+      location.href='../app/index.html';
  </script>";
 }else{
     echo"
       <script>
       alert('登录失败,返回登录页面!!!');
-      location.href='../login.html';
+      location.href='http://10.36.145.33:7777/mingxieku/dist/login.html';
  </script>";
 }
-
-
-
-
-
-
 ?>
--- server/register.php
+++ server/register.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin:*');
 $password = $_POST['password'];
 $tel = $_POST['tel'];
 $code = $_POST['code'];
@@ -10,12 +11,12 @@ $bool = $coon ->query($sql);
 if($bool){
     echo"<script>
              alert('恭喜您注册成功');
-            location.href = '../login.html';
+            location.href = 'http://10.36.145.33:7777/mingxieku/dist/login.html';
         </script>";
 }else{
     echo"<script>
              alert('注册失败!请重新注册');
-            location.href = '../register.html';
+            location.href = 'http://10.36.145.33:7777/mingxieku/dist/register.html';
         </script>";
 }
 ?>
